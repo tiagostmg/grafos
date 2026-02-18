ecfd305dc5bcd7e4b69cbbecfe725d34dbf5fb0d
Tiago Taumaturgo <tiago.taumaturgo@gmail.com>
2026-02-18 16:14:12 -0300

    feat: add histogram

--- src/checkpoint1.py
+++ src/checkpoint1.py
@@ -6,4 +6,15 @@ file = Path(__file__).resolve().parent / "data" / "Email-Enron.txt"
 
 g = new_graph(file)
 
-print_graph(g)
+# print_graph(g)
+
+
+import matplotlib.pyplot as plt
+
+qty_edges = lambda x : int(x.size() / 2)
+
+data = [qty_edges(g.adj[i]) for i in range(g.V)]
+
+plt.hist(data, bins=30, color='skyblue', edgecolor='black')
+
+plt.show()
