1129cc8143c315aa955f32ec24615bb20d1617bb
Tiago Taumaturgo <tiago.taumaturgo@gmail.com>
2026-02-19 20:28:27 -0300

    Refactor graph implementation: Replace algs4 graph with SimpleGraph
    - Updated `new_graph` function to utilize `SimpleGraph` instead of `algs4.graph`.
    - Removed commented-out code related to adjacency list traversal.
    - Introduced `SimpleGraph` class in a new file `simple_graph.py` with methods for adding edges, checking for edges, and printing the graph.
    - Simplified graph representation using an array of sets, allowing for parallel edges and self-loops.

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,12 @@
 from pathlib import Path
-from algs4 import graph
+from simple_graph import SimpleGraph
 
-def new_graph(file: Path) -> graph.Graph:
+def new_graph(file: Path):
   with file.open("r", encoding="utf-8") as f:
     V = int(f.readline())
     E = int(f.readline())
-    g = graph.Graph(V)
+    g = SimpleGraph(V)
     for _ in range(E):
       v, w = f.readline().split()
-      # node = g.adj[int(v)].first
-      # while node is not None:
-      #   if node.item == int(w):
-      #     continue
-      #   node = node.next
       g.add_edge(v, w)
   return g
-
-def print_graph(g: graph.Graph) -> None:
-  print(f"{g.V} vertices, {g.E} edges")
-  for v in range(g.V):
-    neighbors = set()
-    print(f"{v}: ", end="")
-    node = g.adj[v].first
-    while node is not None:
-      print(f"{str(node.item)} ", end="")
-      node = node.next
-    print()
