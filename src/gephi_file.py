f67daa26bc1a9f6ca457097efca23eee139d631b
Tiago Taumaturgo <tiago.taumaturgo@gmail.com>
2026-03-04 20:26:15 -0300

    Add CSV generation from Enron email data
    - Created a new script `gephi_file.py` to read the Enron email data and generate two CSV files: `source-target.csv` and `edges.csv`.
    - Implemented functions to parse the email data and extract edges for graph representation.
    - The generated CSV files are intended for use with Gephi for network analysis.

---
+++ src/gephi_file.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import pandas as pd
+
+file = Path(__file__).resolve().parent / "data" / "Email-Enron.txt"
+
+def generate_csv(file: Path):
+  edges = []
+  with file.open("r", encoding="utf-8") as f:
+    _ = f.readline()
+    E = int(f.readline())
+    for _ in range(E*2):
+      v, w = f.readline().split()
+      v, w = int(v), int(w)
+      edges.append((v, w))
+  return pd.DataFrame(edges, columns=["source", "target"])
+
+def generate_csv_2(file: Path):
+  edges = []
+  with file.open("r", encoding="utf-8") as f:
+    _ = f.readline()
+    E = int(f.readline())
+    for _ in range(E*2):
+      edge = f.readline().strip()
+      edges.append(" ".join(edge.split()))
+  return pd.DataFrame(edges, columns=["edges"])
+
+if __name__ == "__main__":
+  df = generate_csv(file)
+  df.to_csv(file.with_name("source-target.csv"), index=False)
+  print(df.head())
+
+  df2 = generate_csv_2(file)
+  df2.to_csv(file.with_name("edges.csv"), index=False)
+  print(df2.head())
+
+
+
+# Depois que o csv foi gerado, colocamos em https://medialab.github.io/table2net/
+# para gerar o arquivo que o Gephi pode ler.
